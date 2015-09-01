21de2415b5de889485f07caff5ee26b1b4d9f65f
ivailopapazov <papazov.ivaylo@gmail.com>
2015-09-01 14:34:08 +0300

    Implement unit of work design pattern for data layer

---
+++ TripShare/TripShare.Data/ITripShareData.cs
@@ -0,0 +1,21 @@
+﻿namespace TripShare.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using TripShare.Data.Repositories;
+    using TripShare.Models;
+
+
+    public interface ITripShareData
+    {
+        IRepository<User> Users { get; }
+
+        IRepository<Trip> Trips { get; }
+
+        int SaveChanges();
+    }
+}
---
+++ TripShare/TripShare.Data/TripShareData.cs
@@ -0,0 +1,60 @@
+﻿namespace TripShare.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+
+    using TripShare.Data.Repositories;
+    using TripShare.Models;
+
+    public class TripShareData : ITripShareData
+    {
+        private DbContext context;
+        private IDictionary<Type, object> repositories;
+
+        public TripShareData() 
+            : this(new TripShareDbContext())
+        {
+        }
+
+        public TripShareData(DbContext context)
+        {
+            this.context = context;
+            this.repositories = new Dictionary<Type, object>();
+        }
+
+        public IRepository<User> Users
+        {
+            get
+            {
+                return this.GetRepository<User>();
+            }
+        }
+
+        public IRepository<Trip> Trips {
+            get
+            {
+                return this.GetRepository<Trip>();
+            }
+        }
+
+        public int SaveChanges()
+        {
+            return this.context.SaveChanges();
+        }
+
+        private IRepository<T> GetRepository<T>()
+            where T : class
+        {
+            var typeOfRepository = typeof(T);
+
+            if (!this.repositories.ContainsKey(typeOfRepository))
+            {
+                var newRepository = Activator.CreateInstance(typeof(EFRepository<T>), context);
+                this.repositories.Add(typeOfRepository, newRepository);
+            }
+
+            return (IRepository<T>)this.repositories[typeOfRepository];
+        }
+    }
+}
