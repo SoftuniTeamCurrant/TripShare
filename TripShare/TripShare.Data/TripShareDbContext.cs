b0d698568f8f62948524d119eed71d547cfa90db
ivailopapazov <papazov.ivaylo@gmail.com>
2015-09-01 11:18:38 +0300

    Add code first migrations to data layer Set database to sql server with connection string on default instance

--- TripShare/TripShare.Data/TripShareDbContext.cs
+++ TripShare/TripShare.Data/TripShareDbContext.cs
@@ -1,14 +1,18 @@
 ﻿namespace TripShare.Data
 {
+    using System.Data.Entity;
+
     using Microsoft.AspNet.Identity.EntityFramework;
 
     using TripShare.Models;
+    using TripShare.Data.Migrations;
 
     public class TripShareDbContext : IdentityDbContext<User>
     {
         public TripShareDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<TripShareDbContext, Configuration>());
         }
 
         public static TripShareDbContext Create()
