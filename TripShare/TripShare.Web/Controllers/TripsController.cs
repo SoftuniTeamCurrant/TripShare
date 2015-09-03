601030107f795442874c7a154a44d4e43aebf971
ivailopapazov <papazov.ivaylo@gmail.com>
2015-09-03 12:25:03 +0300

    Add ninject to web api project

--- TripShare/TripShare.Web/Controllers/TripsController.cs
+++ TripShare/TripShare.Web/Controllers/TripsController.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using TripShare.Data;
 using TripShare.Models;
 
 namespace TripShare.Web.Controllers
 {
-    public class TripsController : BaseApiController
+    public class TripsController : ApiController
     {
+        public TripsController(ITripShareData data)
+        {
+            this.Data = data;
+        }
+
+        public ITripShareData Data { get; private set; }
+
         public IHttpActionResult GetTrips()
         {
             var cities = this.Data.Cities.All().ToList();
--- TripShare/TripShare.Web/Startup.cs
+++ TripShare/TripShare.Web/Startup.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Owin;
 using Owin;
+using Ninject.Web.Common.OwinHost;
+using Ninject.Web.WebApi.OwinHost;
+using Ninject;
+using System.Reflection;
+using System.Web.Http;
+using TripShare.Data;
 
 [assembly: OwinStartup(typeof(TripShare.Web.Startup))]
 
@@ -13,6 +19,19 @@ namespace TripShare.Web
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            app.UseNinjectMiddleware(CreateKernel).UseNinjectWebApi(GlobalConfiguration.Configuration);
+        }
+
+        private static StandardKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+
+            kernel.Bind<ITripShareData>().To<TripShareData>()
+                .WithConstructorArgument("context", c => new TripShareDbContext());
+
+
+            return kernel;
         }
     }
 }
