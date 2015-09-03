f0357aa288bcd479507872b8c370fc506b77adcd
BarishYumerov <barish.yumerov@abv.bg>
2015-09-03 16:24:35 +0300

    Added adding trips

--- TripShare/TripShare.Web/Controllers/TripsController.cs
+++ TripShare/TripShare.Web/Controllers/TripsController.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.AspNet.Identity;
 using TripShare.Data;
 using TripShare.Models;
+using TripShare.Web.Models.BindingModels;
 using TripShare.Web.Models.ViewModels;
 
 namespace TripShare.Web.Controllers
@@ -22,9 +24,57 @@ namespace TripShare.Web.Controllers
 
         public IHttpActionResult GetTrips()
         {
-            var trips = this.Data.Trips.All().Select(TripViewModel.Create);
+            var trips = this.Data.Trips.All().Select(TripViewModel.Create); //One Query
 
             return this.Ok(trips);
+        }
+
+        public IHttpActionResult PostTrip(AddTripBindingModel model)
+        {
+            if (model == null)
+            {
+                return this.BadRequest("Model cannot be null (no data in request)");
+            }
+
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest(this.ModelState);
+            }
+
+            var departureCity = this.Data.Cities.Find(model.DepartureCityId);
+            var arrivalCity = this.Data.Cities.Find(model.ArrivalCityId);
+
+            if (departureCity == null)
+            {
+                return this.BadRequest("Departure city does not exsist!");
+            }
+
+            if (arrivalCity == null)
+            {
+                return this.BadRequest("Arrival city does not exsist!");
+            }
+
+            var trip = new Trip()
+            {
+                Title = model.Title,
+                ArrivalCityId = model.ArrivalCityId,
+                Description = model.Description,
+                AvailableSeats = model.AvaibleSeats,
+                DepartureCityId = model.DepartureCityId,
+                DepartureDate = model.DepartureDate,
+                DriverId = this.User.Identity.GetUserId()
+            };
+
+            this.Data.Trips.Add(trip);
+            this.Data.SaveChanges();
+
+            var data = this.Data.Trips
+                .All().Where(t => t.Id == trip.Id)
+                .Select(TripViewModel.Create)
+                .FirstOrDefault();
+
+            return this.Ok(data);
+
         }
     }
 }
---
+++ TripShare/TripShare.Web/Models/BindingModels/AddTripBindingModel.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace TripShare.Web.Models.BindingModels
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class AddTripBindingModel
+    {
+        [Required]
+        [MinLength(5)]
+        public string Title { get; set; }
+
+        [Required]
+        [MinLength(10)]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(1, 10, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public byte AvaibleSeats { get; set; }
+
+        public DateTime? DepartureDate { get; set; }
+
+        [Required]
+        public int DepartureCityId { get; set; }
+
+        [Required]
+        public int ArrivalCityId { get; set; }
+    }
+}
