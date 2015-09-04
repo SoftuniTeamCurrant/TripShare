6460a8067837e533b6bb7ca748a961e40bca4997
goodlifeinc <genata93@gmail.com>
2015-09-04 17:36:54 +0300

    Trouble with login authorization / angularJS

--- TripShare/TripShare.Client.Web/Scripts/app.js
+++ TripShare/TripShare.Client.Web/Scripts/app.js
@@ -1,8 +1,20 @@
 ﻿var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', function ($scope, $http) {
-    $http.get('http://localhost:54118/api/trips')
+    $scope.function = $http.get('http://localhost:54118/api/trips')
         .success(function (result) {
             $scope.cities = result;
         });
+
+    $scope.login = function () {
+        var loginData = $scope.LoginData;
+        loginData['grant_type'] = 'password';
+        $http.post('http://localhost:54118/token', loginData, { headers: { "Content-Type": "application/x-www-form-urlencoded" } })
+            .success(function(result) {
+                $scope.UserData = result;
+            })
+            .error(function(err) {
+                console.log(err);
+            });
+    }
 });
