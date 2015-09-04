e83d587b032b20608a87b3495b36dbb6271430c6
BarishYumerov <barish.yumerov@abv.bg>
2015-09-04 12:05:00 +0300

    Added getting trip comments

---
+++ TripShare/TripShare.Web/Controllers/CommentsController.cs
@@ -0,0 +1,27 @@
+﻿namespace TripShare.Web.Controllers
+{
+    using System.Web.Http;
+    using System.Linq;
+    using TripShare.Web.Models.ViewModels;
+    using TripShare.Data;
+
+    [Authorize]
+    public class CommentsController : ApiController
+    {
+        public CommentsController(ITripShareData data)
+        {
+            this.Data = data;
+        }
+
+        public ITripShareData Data { get; private set; }
+
+        // GET api/comments/{id}
+        [HttpGet]
+        public IHttpActionResult GetComments(int id)
+        {
+            var comments = this.Data.Trips.Find(id).Comments.AsQueryable().Select(CommentViewModel.Create);
+
+            return this.Ok(comments);
+        }
+    }
+}
---
+++ TripShare/TripShare.Web/Models/ViewModels/CommentViewModel.cs
@@ -0,0 +1,37 @@
+﻿namespace TripShare.Web.Models.ViewModels
+{
+    using System;
+    using System.Linq.Expressions;
+    using TripShare.Models;
+
+    public class CommentViewModel
+    {
+        public int Id { get; set; }
+
+        public string Author { get; set; }
+
+        public string Content { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public int TripId { get; set; }
+
+        public DateTime PostedOn { get; set; }
+
+        public static Expression<Func<Comment, CommentViewModel>> Create
+        {
+            get
+            {
+                return p => new CommentViewModel()
+                {
+                    Id = p.Id,
+                    Author = p.Author.UserName,
+                    Content = p.Content,
+                    AuthorId = p.AuthorId,
+                    TripId = p.TripId,
+                    PostedOn = p.PostedOn
+                };
+            }
+        }
+    }
+}
