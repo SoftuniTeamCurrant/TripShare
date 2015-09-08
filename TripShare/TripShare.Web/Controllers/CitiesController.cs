c4797f37beb9c725a60026c89c41617dbd39d039
swillen <swillen@abv.bg>
2015-09-08 10:52:44 +0300

    Added CityController
    The controller is going to be used for filling dropdowns in the search.

---
+++ TripShare/TripShare.Web/Controllers/CitiesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TripShare.Data;
+
+namespace TripShare.Web.Controllers
+{
+    
+    public class CitiesController : BaseApiController
+    {
+        [HttpGet]
+        [Route("api/cities")]
+        public IHttpActionResult GetCities()
+        {
+            var citiesList = this.Data.Cities.All().Select(city => city.Name).ToList();
+
+            return this.Ok(citiesList);
+
+        }
+    }
+}
